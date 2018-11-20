781211cb65518c3977dbf8c61f366147862b3739
aleksanderpiron <pironaleksander@gmail.com>
2018-11-20 08:37:21 +0100

    test

--- js/app.js
+++ js/app.js
@@ -9,3 +9,12 @@ $('#filters_mask').click(function(){
     $(filters_elem_array).toggleClass('showed');
     $('body').toggleClass('freeze');
 });
+
+$('#create_test_btn').click(function(){
+    $(".add-new-component").addClass("hide");
+    $('.add-new-test').addClass('visible');
+})
+$('#new_test_cancel').click(function(){
+    $(".add-new-component").removeClass("hide");
+    $('.add-new-test').removeClass('visible');
+})
