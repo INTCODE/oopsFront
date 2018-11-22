9220a0b073000e5b869ad334c46d847ed5fb5b44
aleksanderpiron <pironaleksander@gmail.com>
2018-11-22 08:38:41 +0100

    up

---
+++ konto rekrutera/js/app.js
@@ -0,0 +1,20 @@
+var filters_btn = $('#filters_toggle');
+var filters_elem_array = [$('.filters'),$('#filters_mask')];
+
+$(filters_btn).click(function(){
+    $(filters_elem_array).toggleClass('showed');
+    $('body').toggleClass('freeze');
+});
+$('#filters_mask').click(function(){
+    $(filters_elem_array).toggleClass('showed');
+    $('body').toggleClass('freeze');
+});
+
+$('#create_test_btn').click(function(){
+    $(".add-new-component").addClass("hide");
+    $('.add-new-test').addClass('visible');
+})
+$('#new_test_cancel').click(function(){
+    $(".add-new-component").removeClass("hide");
+    $('.add-new-test').removeClass('visible');
+})
