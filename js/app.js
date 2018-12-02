0f81c55ad456295e8990c5f51739ecf142edd92c
aleksanderpiron <pironaleksander@gmail.com>
2018-12-02 13:21:31 +0100

    poprawki

--- js/app.js
+++ js/app.js
@@ -22,3 +22,52 @@ $('#new_test_cancel').click(function(){
     $(".add-new-component").removeClass("hide");
     $('.add-new-test').removeClass('visible');
 })
+
+// MOBILE EMPLOYERS ADD
+if($(document).width()<991){
+    $('#dragula_left .empty-slot').on('click',function(){
+        $("#dragula_choose_box").toggleClass('showed');
+        $('body').toggleClass('freeze');
+    });
+    $('#dragula_choose_cancel').on('click',function(){
+        $("#dragula_choose_box").toggleClass('showed');
+        $('body').toggleClass('freeze');
+    });
+    $('#dragula_right .recommended-user').on('click', function(){
+        $(this).prependTo("#dragula_left");
+        $(this).off();
+        $('.empty-slot').remove();
+        if($("#dragula_left").children().length<10){
+            var missing_empty = 10 - $("#dragula_left").children().length;
+            for(i=0;i<missing_empty;i++){
+                $(emptySlot).appendTo("#dragula_left");
+                $('#dragula_left .empty-slot').on('click',function(){
+                    $("#dragula_choose_box").toggleClass('showed');
+                    $('body').toggleClass('freeze');
+                });
+            }
+        }
+        $('#dragula_left .recommended-user').on('click', function(){
+            $(this).prependTo("#dragula_right");
+            $(this).off();
+            if($("#dragula_left").children().length<10){
+                $(emptySlot).appendTo("#dragula_left");
+            }
+            $('#dragula_right .recommended-user').on('click', function(){
+                $(this).prependTo("#dragula_left");
+                $(this).off();
+                $('.empty-slot').remove();
+                if($("#dragula_left").children().length<10){
+                    var missing_empty = 10 - $("#dragula_left").children().length;
+                    for(i=0;i<missing_empty;i++){
+                        $(emptySlot).appendTo("#dragula_left");
+                        $('#dragula_left .empty-slot').on('click',function(){
+                            $("#dragula_choose_box").toggleClass('showed');
+                            $('body').toggleClass('freeze');
+                        });
+                    }
+                }
+            });
+        })
+    })
+}
